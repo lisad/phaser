384b69f42f9de88ae50ca3e4ec918bf04638568c
Lisa Dusseault <lisa@rtfm.com>
2024-01-11 15:07:58 -0800

    Refactors step decorators: Makes probing steps for what to pass them a better design, and changes the parameters to pass in a context rather than the phase itself

--- phaser/steps.py
+++ phaser/steps.py
@@ -1,15 +1,19 @@
 from functools import wraps
 
+ROW_STEP = "ROW_STEP"
+BATCH_STEP = "BATCH_STEP"
+DATAFRAME_STEP = "DATAFRAME_STEP"
+PROBE_VALUE = "__PROBE__"
 
 def row_step(step_function):
     """ This decorator is used to indicate a step that should run on each row of a data set.
     It adds a "probe" response to the step that allows the phase running logic to know this.
     """
     @wraps(step_function)
-    def _row_step_wrapper(phase, row):
-        if row == "__PROBE__":
-            return "row_step"  # Allows Phase to probe a step for how to call it
-        result = step_function(phase, row)
+    def _row_step_wrapper(row, context=None, __probe__=None):
+        if __probe__ == PROBE_VALUE:
+            return ROW_STEP  # Allows Phase to probe a step for how to call it
+        result = step_function(row, context=context)
         assert isinstance(result, dict)
         return result
     return _row_step_wrapper
@@ -20,10 +24,10 @@ def batch_step(step_function):
     whole batch by adding a 'probe' response
     """
     @wraps(step_function)
-    def _batch_step_wrapper(phase, batch):
-        if batch == "__PROBE__":
-            return "batch_step"
-        result = step_function(phase, batch)
+    def _batch_step_wrapper(batch, context=None, __probe__=None):
+        if __probe__ == PROBE_VALUE:
+            return BATCH_STEP
+        result = step_function(batch, context=context)
         assert isinstance(result, list)
         return result
     return _batch_step_wrapper
@@ -40,7 +44,7 @@ def check_unique(column_name, strip=True, ignore_case=False):
     """
 
     @batch_step
-    def check_unique_step(phase, batch):
+    def check_unique_step(batch, **kwargs):
         values = [row.get(column_name) for row in batch]
         if strip:
             values = [value.strip() for value in values]
