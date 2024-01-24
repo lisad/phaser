1d567925fa42f9c5b7ec948c50359cf09e35078c
Jeff Kolesky <jeff@kolesky.com>
2024-01-23 21:31:50 -0800

    Uses argparse for argument parsing

--- phaser/__main__.py
+++ phaser/__main__.py
@@ -3,15 +3,25 @@
 # This is the prototype from which we can build more subcommands to use for
 # running various tasks as part of a data integration system.
 
-import sys
+import argparse
 from importlib import import_module
 import inspect
 import phaser
 
 class PipelineRunner:
-    def __init__(self, argv=None):
-        self.argv = argv or sys.argv[:]
-        self.pipeline_name = self.argv[1]
+    def cmd_name():
+        return "run"
+
+    def cmd_help():
+        return "run a pipeline"
+
+    def add_arguments(parser):
+        parser.add_argument("pipeline_name", help="pipeline to run")
+        parser.add_argument("working_dir", help="directory to output phase results")
+        parser.add_argument("source", help="file to use as initial source")
+
+    def __init__(self, args):
+        self.pipeline_name = args.pipeline_name
         # Pipelines are expected to be defined in a module in the `pipelines`
         # package. The module name is given as the command line argument, and
         # the sole subclass of phaser.Pipeline is located and invoked with
@@ -27,8 +37,8 @@ class PipelineRunner:
         # pipelines is a tuple of names and values. We want the value which is
         # a class object.
         self.Pipeline = pipelines[0][1]
-        self.working_dir = self.argv[2]
-        self.source = self.argv[3]
+        self.working_dir = args.working_dir
+        self.source = args.source
 
     def __is_pipeline_class(obj):
         # isinstance(attr, type) is the way to check that the attr is a class
@@ -42,7 +52,15 @@ class PipelineRunner:
         pipeline.run()
 
 def main():
-    runner = PipelineRunner(sys.argv)
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+    commands = [ PipelineRunner ]
+    for command in commands:
+        subparser = subparsers.add_parser(command.cmd_name(), help=command.cmd_help())
+        command.add_arguments(subparser)
+    args = parser.parse_args()
+
+    runner = PipelineRunner(args)
     runner.execute()
 
 if __name__ == '__main__':
