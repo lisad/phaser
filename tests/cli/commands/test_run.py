a10bbc3bc1cf3bc6a39d30fc69d30326bbcfe978
Jeff Kolesky <jeff@kolesky.com>
2024-01-29 11:23:03 -0800

    Adds a test for the happy path
    Also... uses test tables instead of repetitive code for the failure
    scenarios

--- tests/cli/commands/test_run.py
+++ tests/cli/commands/test_run.py
@@ -1,11 +1,12 @@
 import argparse
+import filecmp
 from pathlib import Path
 import os
 import pytest
 import phaser.cli
 
+test_dir = Path(__file__).parent
 current_path = Path(__file__).parent.parent.parent
-print(f"{current_path}")
 
 def __build_command():
     parser = argparse.ArgumentParser()
@@ -13,70 +14,57 @@ def __build_command():
     command.add_arguments(parser)
     return (parser, command)
 
-def test_runs_a_pipeline():
-    pass
-
-def test_no_pipeline_package_found(tmpdir):
-    """
-    No pipelines packaage is found, because the test is run from the main
-    directory, which has no "pipelines" subdirectory
-    """
-    (parser, command) = __build_command()
-    source = current_path / "fixture_files" / "crew.csv"
-    args = parser.parse_args(f"pipelinerunnerpipeline {tmpdir} {source}".split())
-    with pytest.raises(ModuleNotFoundError):
+def test_runs_a_pipeline(tmpdir):
+    cwd = Path(os.getcwd())
+    try:
+        os.chdir(test_dir)
+        (parser, command) = __build_command()
+        source = current_path / "fixture_files" / "runner-test.csv"
+        args = parser.parse_args(f"passthrough {tmpdir} {source}".split())
         command.execute(args)
-
-def test_no_pipeline_module_found(tmpdir):
-    """
-    Test is run from the testing directory that has a "pipelines" package, but
-    there is no matching module.
-    """
-    cwd = Path(os.getcwd())
-    try:
-        os.chdir(Path(__file__).parent)
-        (parser, command) = __build_command()
-        source = current_path / "fixture_files" / "crew.csv"
-        args = parser.parse_args(f"doesnotexist {tmpdir} {source}".split())
-        with pytest.raises(ModuleNotFoundError):
-            command.execute(args)
+        # Check that the output in the tmpdir is exactly the same as the input
+        assert filecmp.cmp(Path(tmpdir) / "passthrough_output_runner-test.csv", source)
     finally:
-        # Be sure to change back to the prior working directory
         os.chdir(cwd)
 
-def test_no_pipeline_found(tmpdir):
-    """
-    Test is run from the testing directory that has a "pipelines" package, but
-    there is pipeline class declared in the module
-    """
-    cwd = Path(os.getcwd())
-    try:
-        os.chdir(Path(__file__).parent)
-        (parser, command) = __build_command()
-        source = current_path / "fixture_files" / "crew.csv"
-        args = parser.parse_args(f"nopipelinehere {tmpdir} {source}".split())
-        with pytest.raises(Exception):
-            command.execute(args)
-    finally:
-        # Be sure to change back to the prior working directory
-        os.chdir(cwd)
-
-def test_multiple_pipelines_found(tmpdir):
-    """
-    Test is run from the testing directory that has a "pipelines" package, but
-    there are multiple pipeline classes declared in the module
-    """
-    cwd = Path(os.getcwd())
-    try:
-        os.chdir(Path(__file__).parent)
-        (parser, command) = __build_command()
-        source = current_path / "fixture_files" / "crew.csv"
-        args = parser.parse_args(f"multiplepipelines {tmpdir} {source}".split())
-        with pytest.raises(Exception):
-            command.execute(args)
-    finally:
-        # Be sure to change back to the prior working directory
-        os.chdir(cwd)
+def test_failure_scenarios(tmpdir):
+    # Test tables are helpful for reducing code, but I am not sure how to get
+    # the name of the test case that failed when a test case fails. In other
+    # ecosystems, the assertions accept a message upon failure that I would pass
+    # the case name into.
+    tests = [{
+        "name": "No pipeline package found",
+        "pipeline": "pipelinerunnerpipeline",
+        "exception": ModuleNotFoundError
+    }, {
+        "name": "No pipeline module found",
+        "cwd": test_dir,
+        "pipeline": "doesnotexist",
+        "exception": ModuleNotFoundError
+    }, {
+        "name": "No pipeline found",
+        "cwd": test_dir,
+        "pipeline": "nopipelinehere",
+        "exception": Exception
+    }, {
+        "name": "Multiple pipelines found",
+        "cwd": test_dir,
+        "pipeline": "multiplepipelines",
+        "exception": Exception
+    }]
+    for test in tests:
+        cwd = Path(os.getcwd())
+        try:
+            if test.get('cwd'):
+                os.chdir(test['cwd'])
+            (parser, command) = __build_command()
+            source = current_path / "fixture_files" / "crew.csv"
+            args = parser.parse_args(f"{test['pipeline']} {tmpdir} {source}".split())
+            with pytest.raises(test['exception']):
+                command.execute(args)
+        finally:
+            # Be sure to return to the directory we were at at the beginning
+            os.chdir(cwd)
 
 def test_overrides_working_directory():
     pass
