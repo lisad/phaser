de247aeb8ab4472f689c892efc22c31df5ad0a72
Jeff Kolesky <jeff@kolesky.com>
2024-01-29 17:03:10 -0800

    Parameterizes the failure scenario test

--- tests/test_command_run.py
+++ tests/test_command_run.py
@@ -20,34 +20,20 @@ def test_runs_a_pipeline(tmpdir):
     # Check that the output in the tmpdir is exactly the same as the input
     assert filecmp.cmp(Path(tmpdir) / "passthrough_output_runner-test.csv", source)
 
-def test_failure_scenarios(tmpdir):
-    # Test tables are helpful for reducing code, but I am not sure how to get
-    # the name of the test case that failed when a test case fails. In other
-    # ecosystems, the assertions accept a message upon failure that I would pass
-    # the case name into.
-    tests = [{
-        "name": "No pipeline package found",
-        "pipeline": "pipelinerunnerpipeline",
-        "exception": ModuleNotFoundError
-    }, {
-        "name": "No pipeline module found",
-        "pipeline": "doesnotexist",
-        "exception": ModuleNotFoundError
-    }, {
-        "name": "No pipeline found",
-        "pipeline": "nopipelinehere",
-        "exception": Exception
-    }, {
-        "name": "Multiple pipelines found",
-        "pipeline": "multiplepipelines",
-        "exception": Exception
-    }]
-    for test in tests:
-        (parser, command) = __build_command()
-        source = current_path / "fixture_files" / "crew.csv"
-        args = parser.parse_args(f"{test['pipeline']} {tmpdir} {source}".split())
-        with pytest.raises(test['exception']):
-            command.execute(args)
+@pytest.mark.parametrize("pipeline,exception",
+    [
+        ("pipelinerunnerpipeline", ModuleNotFoundError),
+        ("doesnotexist", ModuleNotFoundError),
+        ("nopipelinehere", Exception),
+        ("multiplepipelines", Exception),
+    ]
+)
+def test_failure_scenarios_p(tmpdir, pipeline, exception):
+    (parser, command) = __build_command()
+    source = current_path / "fixture_files" / "crew.csv"
+    args = parser.parse_args(f"{pipeline} {tmpdir} {source}".split())
+    with pytest.raises(exception):
+        command.execute(args)
 
 def test_overrides_working_directory():
     pass
