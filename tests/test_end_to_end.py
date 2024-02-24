a594faf563c370e1d66ba67ef3ac2ac2bfba1b07
Lisa Dusseault <lisa@rtfm.com>
2024-02-23 21:28:19 -0800

    Fixes demo employee pipeline that wasn't fully tested in end-to-end tests

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -1,4 +1,6 @@
 import pytest
+import os
+import pandas
 from pathlib import Path
 from pipelines.employees import EmployeeReviewPipeline
 
@@ -8,12 +10,15 @@ current_path = Path(__file__).parent
 def test_employee_pipeline(tmpdir):
     source = current_path / "fixture_files" / "employees.csv"
     EmployeeReviewPipeline(source=source, working_dir=tmpdir).run()
+    assert os.path.exists(tmpdir / 'Validator_output_employees.csv')
+    assert os.path.exists(tmpdir / 'Transformer_output_employees.csv')
 
 
-@pytest.mark.skip("work in progress")
-def test_employee_pipeline_calculates_bonus():
-    assert False
-    # LMDTODO: Make sure bonuses are calculated correctly! etc!
+def test_employee_pipeline_calculates_bonus(tmpdir):
+    source = current_path / "fixture_files" / "employees.csv"
+    EmployeeReviewPipeline(source=source, working_dir=tmpdir).run()
+    new_data = pandas.read_csv(tmpdir / 'Transformer_output_employees.csv').to_dict(orient='records')
+    assert all([row['Bonus percent'] > 0.1 and row['Bonus percent'] < 0.2 for row in new_data])
 
 
-    # ALso add tests that introduce errors to employee data & run pipeline or phases?
+# ALso add tests that introduce errors to employee data & run pipeline or phases?
