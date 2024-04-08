78aa02a820c12737f0f7d341b6b860431ed31f29
Jeff Kolesky <jeff@kolesky.com>
2024-04-08 09:49:29 -0700

    Updates test to work with IOObjects

--- tests/test_extra_ios.py
+++ tests/test_extra_ios.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 import pytest
 from phaser import Phase, context_step, Pipeline
+from phaser.io import ExtraRecords, read_csv
 
 current_path = Path(__file__).parent
 
@@ -9,7 +10,7 @@ current_path = Path(__file__).parent
 
 @context_step
 def add_an_output(context):
-    context.set_output('doctors', [{'on_call': "Julian Bashir"}])
+    context.set_output('doctors', ExtraRecords('doctors', data=[{'on_call': "Julian Bashir"}]))
 
 class EmployeePhase(Phase):
     steps = [
@@ -25,6 +26,12 @@ def test_extra_outputs(tmpdir):
     pipeline = MyPipeline(working_dir=tmpdir)
     pipeline.run()
     assert os.path.exists(tmpdir / 'doctors.csv')
+    # We are checking for the full text of the file rather than using read_csv
+    # to read the data back in and assert against it, because read_csv fails to
+    # detect the csv dialect when there is only a single field (or maybe it is
+    # when there is not enough data).
+    text = (tmpdir / 'doctors.csv').read_text(encoding='utf8')
+    assert text == 'on_call\nJulian Bashir\n'
 
 
 @pytest.mark.skip("This doesn't work yet - the pipeline tells the context when to save extra outputs")
