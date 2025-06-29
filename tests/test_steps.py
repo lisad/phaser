32cbec3b24cc815587f739c337e65c3cffcbaf44
Lisa Dusseault <lisa@rtfm.com>
2025-06-29 10:49:45 -0700

    Fix unit tests previously broken

--- tests/test_steps.py
+++ tests/test_steps.py
@@ -5,11 +5,10 @@ import pandas as pd
 import pytest
 from unittest.mock import patch
 
-from phaser import (Phase, row_step, batch_step, context_step,
+from phaser import (Phase, row_step, batch_step, context_step, Context,
                     DropRowException, PhaserError, dataframe_step,
-                    PHASER_ROW_NUM, ON_ERROR_STOP_NOW)
+                    PHASER_ROW_NUM, ON_ERROR_STOP_NOW, records)
 from phaser.io import ExtraMapping
-import phaser
 from steps import sum_bonuses
 
 current_path = Path(__file__).parent
@@ -114,13 +113,14 @@ def test_row_step_wrong_return_type_errors():
     def step_returns_value_other_than_row(row, context):
         return context.get('return_value')
 
-    with patch.object(phaser.Context, 'get', return_value=1):
-        with pytest.raises(PhaserError) as exc_info:
-            step_returns_value_other_than_row({'data': 'testrow'}, phaser.Context())
+    context = Context()
+    context.add_variable('step_return_value', 1)
+    with pytest.raises(PhaserError) as exc_info:
+        step_returns_value_other_than_row({'data': 'testrow'}, context)
         assert "return row in dict format, not 1" in exc_info.value.message
-    with patch.object(phaser.Context, 'get', return_value=[]):
-        with pytest.raises(PhaserError) as exc_info:
-            step_returns_value_other_than_row({'data': 'testrow'}, phaser.Context())
+    context.add_variable('step_return_value', 1)
+    with pytest.raises(PhaserError) as exc_info:
+        step_returns_value_other_than_row({'data': 'testrow'}, context)
         assert "return row in dict format, not []" in exc_info.value.message
 
 # Batch steps
@@ -272,13 +272,13 @@ def test_context_step_keeps_numbers():
     def a_step(context):
         assert 1 > 0
 
-    row_num_gen = phaser.records.row_num_generator()
+    row_num_gen = records.row_num_generator()
     # PIpeline will set up Records that converts __phaser_row_num__ into record.row_num.  Replicate that setup
     # so we can see that it keeps that setup.
     data_with_row_numbers = [
         {'id': 3, 'age': 48, PHASER_ROW_NUM: 3}
     ]
-    data = phaser.records.Records(data_with_row_numbers, row_num_gen)
+    data = records.Records(data_with_row_numbers, row_num_gen)
     phase = Phase("test", steps=[a_step])
     phase.load_data(data)
     # Before running, the 2nd row, index 1, should keep the # 3... and after running also
